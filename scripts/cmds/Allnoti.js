f29439aa4b8fabd8664b642094923836a85d4cdf
balerbot789 <balerbot789@gmail.com>
2026-08-15 03:50:42 +0600

    Update Allnoti.js

--- scripts/cmds/Allnoti.js
+++ scripts/cmds/Allnoti.js
@@ -43,7 +43,7 @@ module.exports = {
 
           await api.sendMessage(
             {
-              body: `🔔 𝙉𝙊𝙏𝙄𝙁𝙄𝘾𝘼𝙏𝙄𝙊𝙉\n━━━━━━━━━━━━━━━\n📢 From Owner: 〲MAMUNツ࿐ T.T o.O\n\n${msg}\n━━━━━━━━━━━━━━━`,
+              body: `🔔 𝙉𝙊𝙏𝙄𝙁𝙄𝘾𝘼𝙏𝙄𝙊𝙉\n━━━━━━━━━━━━\n📢 From Owner: 〲MAMUNツ࿐ T.T o.O\n\n${msg}\n━━━━━━━━━━━━`,
               attachment: attachment
             },
             thread.threadID
@@ -57,7 +57,7 @@ module.exports = {
     }
 
     api.sendMessage(
-      `✅ Done Owner Broadcast\n✔️ Success: ${success}\n❌ Failed: ${failed}`,
+      `sent notification from \n✔️ Success: ${success}\n❌ Failed: ${failed}`,
       event.threadID
     );
   }
