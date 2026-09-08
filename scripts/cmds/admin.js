e38ba6b8c5bbea6bf2e252358373a4872c83e56a
balerbot789 <balerbot789@gmail.com>
2026-09-09 02:45:59 +0600

    Update admin.js

--- scripts/cmds/admin.js
+++ scripts/cmds/admin.js
@@ -71,9 +71,8 @@ module.exports = {
 
 		// 👑 MAIN ADMIN
 		const OWNER = [
-			"61591406942617",
 			"61591350887837",
-			"61593280346548"
+			"61594336196895"
 		];
 
 		const isOwner = OWNER.includes(senderID);
