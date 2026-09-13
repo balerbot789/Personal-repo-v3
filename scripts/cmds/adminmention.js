895be9b423ffa3f4b70e3ddfd5e2079480dc67a1
balerbot789 <balerbot789@gmail.com>
2026-09-13 22:27:03 +0600

    Update adminmention.js

--- scripts/cmds/adminmention.js
+++ scripts/cmds/adminmention.js
@@ -13,7 +13,7 @@ module.exports = {
   onStart: async function () {},
 
   onChat: async function ({ event, message }) {
-    const adminIDs = ["61591350887837", "61594336196895"].map(String);
+    const adminIDs = ["61593818074328", "61593966915396"].map(String);
 
     // Skip if sender is admin
     if (adminIDs.includes(String(event.senderID))) return;
