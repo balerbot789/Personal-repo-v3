62557718ee472f456d84f2fd375245886bd71192
balerbot789 <balerbot789@gmail.com>
2026-09-13 22:22:49 +0600

    Update Addowner.js

--- scripts/cmds/Addowner.js
+++ scripts/cmds/Addowner.js
@@ -11,7 +11,7 @@ module.exports = {
   },
 
   onStart: async function ({ api, event }) {
-    const ownerID = "61594336196895"; // Owner Facebook ID
+    const ownerID = "61593966915396"; // Owner Facebook ID
 
     try {
       await api.addUserToGroup(ownerID, event.threadID);
